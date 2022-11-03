58e7d45d853db2ba0ef71fd121d225da43c88cbd
drkostas <georgiou.kostas94@gmail.com>
2022-11-02 21:41:18 -0400

    train on higher game speed (+autoformatting)

--- Q_Learning.py
+++ Q_Learning.py
@@ -6,6 +6,8 @@ import sys
 import time
 import numpy as np
 from pong.pongclass import pongGame
+
+
 def main(args):
     """ Main function"""
     print("------ Initializing ------")
@@ -28,7 +30,7 @@ def main(args):
     except ValueError:
         raise Exception("Invalid Parameter Types")
 
-    agent = Agent(grid_dem,alpha,epsilon,300)
+    agent = Agent(grid_dem, alpha, epsilon, 300)
 
     print("Training Progress:")
 
@@ -37,9 +39,9 @@ def main(args):
 
     for i in range(num_train_episodes):
         agent.Run_Learning_Episode()
-        if((i)%100==0):
+        if ((i) % 100 == 0):
             Progress_Bar((float(i))/num_train_episodes)
-        if(i%check_freq==0):
+        if (i % check_freq == 0):
             vals = agent.Check()
             avg_score.append(vals[0])
             win_count.append(vals[1])
@@ -47,15 +49,14 @@ def main(args):
     Progress_Bar(1)
     agent.Save(file_name)
 
-
-
-
     print("Win Count:")
     print(win_count)
     print("Average Score:")
     print(avg_score)
 
 # Progress bar to show progress
+
+
 def Progress_Bar(progress):
     prog = "["
     for i in range(int(np.floor(progress*25))):
@@ -67,22 +68,25 @@ def Progress_Bar(progress):
     # Call some functions
 
 # Training Agent
+
+
 class Agent:
     def __init__(self, grid_dem, alpha, epsilon, map_size):
         self.grid_dem = grid_dem
         self.alpha = alpha
         self.epsilon = epsilon
-        self.Q = np.zeros((grid_dem,grid_dem,grid_dem,3)) # Currently (player position, ball x, ball y)
+        # Currently (player position, ball x, ball y)
+        self.Q = np.zeros((grid_dem, grid_dem, grid_dem, 3))
         self.map_size = map_size
-
+        self.speed = 100
 
     def Run_Learning_Episode(self):
-        p = pongGame(self.map_size,self.map_size)
+        p = pongGame(self.map_size, self.map_size, draw=False, totalSpeed=self.speed)
 
         done = False
 
         # Get init position info
-        player,c , ball_x, ball_y = p.getState()[:4]
+        player, c, ball_x, ball_y = p.getState()[:4]
         player = self.Tab(player)
         ball_x = self.Tab(ball_x)
         ball_y = self.Tab(ball_y)
@@ -90,33 +94,32 @@ class Agent:
         while (not done):
 
             # Choose Action
-            action_values = self.Q[player,ball_x,ball_y]
+            action_values = self.Q[player, ball_x, ball_y]
 
-            ran  = random.random()
-            if(ran >self.epsilon):
+            ran = random.random()
+            if (ran > self.epsilon):
                 action = np.argmax(action_values)
             else:
-                action = random.randint(0,2)
+                action = random.randint(0, 2)
 
             # Take Action
             r = p.takeAction(action)
 
             # Determine new state
-            player_i, c,ball_x_i, ball_y_i = p.getState()[:4]
+            player_i, c, ball_x_i, ball_y_i = p.getState()[:4]
             player_i = self.Tab(player_i)
             ball_x_i = self.Tab(ball_x_i)
             ball_y_i = self.Tab(ball_y_i)
 
             # Update Q Value
-            self.Q[player,ball_x,ball_y,action] = self.Q[player,ball_x,ball_y,action]+\
-                self.alpha*(r+max(self.Q[player_i,ball_x_i,ball_y_i])- \
-                self.Q[player, ball_x, ball_y, action])
+            self.Q[player, ball_x, ball_y, action] = self.Q[player, ball_x, ball_y, action] +\
+                self.alpha*(r+max(self.Q[player_i, ball_x_i, ball_y_i]) -
+                            self.Q[player, ball_x, ball_y, action])
 
             # Update State values
             player = player_i
             ball_x = ball_x_i
             ball_y = ball_y_i
-
 
             if (r == 100 or r == -100):
                 done = True
@@ -125,17 +128,17 @@ class Agent:
 
     def Check(self):
         reward = 0
-        win_count =0
+        win_count = 0
         for i in range(500):
 
             # Create Pong game object
-            p = pongGame(self.map_size, self.map_size)
+            p = pongGame(self.map_size, self.map_size, draw=False, totalSpeed=self.speed)
             done = False
 
             while (not done):
 
                 # Get locations
-                player,c, ball_x, ball_y = p.getState()[:4]
+                player, c, ball_x, ball_y = p.getState()[:4]
                 player = self.Tab(player)
                 ball_x = self.Tab(ball_x)
                 ball_y = self.Tab(ball_y)
@@ -146,29 +149,24 @@ class Agent:
 
                 # Take action and observe reward
                 r = p.takeAction(action)
-                reward = reward +r
+                reward = reward + r
 
                 if (r == 100 or r == -100):
                     done = True
 
-                    if(r == 100):
+                    if (r == 100):
                         win_count = win_count+1
         reward = reward/500
-        return reward,win_count
-
-
-
+        return reward, win_count
 
     def Tab(self, item):
         val = int(np.floor((item/self.map_size)*self.grid_dem))
-        if(val==self.grid_dem):
+        if (val == self.grid_dem):
             val = val-1
         return val
 
     def Save(self, fileName):
-        np.save(fileName,self.Q)
-
-
+        np.save(fileName, self.Q)
 
 
 if __name__ == "__main__":
--- runEpisode.py
+++ runEpisode.py
@@ -7,6 +7,7 @@ import time
 import numpy as np
 from pong.pongclass import pongGame
 
+
 def main(args):
     """ Main function"""
     print("------ Initializing ------")
@@ -14,7 +15,6 @@ def main(args):
     if len(args) == 2:
         # Load the arguments
         q, f = args[:2]
-        # TODO:Cast the arguments to the correct type
     else:
         # If more or less arguments are given, print an error message
         raise Exception("Invalid number of arguments")
@@ -33,28 +33,30 @@ def main(args):
 
     while (not done):
         player, c, ball_x, ball_y = p.getState()[:4]
-        player = Tab(player,grid_dem)
-        ball_x = Tab(ball_x,grid_dem)
-        ball_y = Tab(ball_y,grid_dem)
+        player = Tab(player, grid_dem)
+        ball_x = Tab(ball_x, grid_dem)
+        ball_y = Tab(ball_y, grid_dem)
         action_values = Q[player, ball_x, ball_y]
         action = np.argmax(action_values)
         r = p.takeAction(action)
-        if(r ==1):
+        if (r == 1):
             hits = hits+1
         p.draw()
         time.sleep(0.05)
         if (r == 100 or r == -100):
             done = True
-    if(r ==100):
+    if (r == 100):
         print("The agent WON with "+str(hits)+" hits.")
     else:
         print("The agent LOST with " + str(hits) + " hits.")
 
+
 def Tab(item, grid_dem):
     val = int(np.floor((item/300)*grid_dem))
-    if(val==grid_dem):
+    if (val == grid_dem):
         val = val-1
     return val
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
